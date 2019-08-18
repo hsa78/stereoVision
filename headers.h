fb958bc359eef6602a2f5ba3763335aafd45f148
Mahdi <49525652+SMahdiHosseini@users.noreply.github.com>
2019-08-18 18:43:46 +0430

    delete one class and add a method to Stereo
    با توجه به صورت پروژه و دریافت مطالب جدید یکی از کلاس ها پاک شد و در ازای آن یک متد جدید به کلاس اصلی اضافه شد

--- headers.h
+++ headers.h
@@ -5,19 +5,14 @@
 class d_pixel
 {
 public:
-	d_pixel(int _x, int _y, int _disparity) : x(_x), y(_y), disparity(_disparity) {};
+	d_pixel(int _x, int _y, int _disparity) : x(_x), y(_y), disparity(_disparity) {}
+	int get_x() {return x;}
+	int get_y() {return	y;}
+	int get_disparity() {return disparity;}
 private:
 	int x;
 	int y;
 	int disparity;
-};
-
-class Cost
-{
-public:
-	Cost(cv::Mat image_1, cv::Mat image_2){};
-private:
-	std::vector<std::vector<std::vector<int>>> ssd;
 };
 
 
@@ -25,14 +20,15 @@ class Stereo
 {
 public:
 	Stereo(cv::Mat image_1, cv::Mat image_2, int _max_disparity, int _w);
+	cv::Mat get_result() {return result;}
 	void compute();
-	int total_cost(int x, int y);
+	void set_pixel(d_pixel disparity);
+	int total_cost(int x, int y, /**/int d);
 	d_pixel disparity(int x, int y);
 private:
 	cv::Mat image_l;
 	cv::Mat image_r;
 	cv::Mat result;
-	Cost ssd;
 	int max_disparity;
 	int w;
 };
