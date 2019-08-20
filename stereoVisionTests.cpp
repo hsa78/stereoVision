851804621ab67e50aff9b9b687923491cabfd0eb
hsazarmsa <hsazarmsa@gmail.com>
2019-08-20 19:02:16 +0430

    add code for makeImgVectorWithAlgorithmType1 function

--- stereoVisionTests.cpp
+++ stereoVisionTests.cpp
@@ -5,5 +5,23 @@ ImgMaker::ImgMaker(int _algorithmType,int _rowsNum = IMG_ROWS_NUMBER,int _colsNu
     algorithmType = _algorithmType;
     rowsNum = max(_rowsNum,0);
     colsNum = max(_colsNum,0);
+    img = Mat::zeros(rowsNum,colsNum,CV_8UC1);
 }
 
+vector<vector<int> > ImgMaker::makeImgVectorWithAlgorithmType1()
+{
+    vector<vector<int> > imgValues;
+
+    for(int i = 0; i < rowsNum; i++)
+    {
+        vector<int> temp;
+        imgValues.push_back(temp);
+
+        for(int j = 0; j < colsNum; j++)
+        {
+            imgValues[i].push_back((i + j) % MAX_PIXEL_VALUE);
+        }
+    }
+
+    return imgValues;
+}
